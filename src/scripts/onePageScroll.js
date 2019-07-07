68107cb26cc8ddb6d1ba3a8db8fe8c4c25f60915
MrBlond1n <nikita.kirdiapin@gmail.com>
2019-07-07 16:09:51 +0300

    добавление кнопок для навигации

---
+++ src/scripts/onePageScroll.js
@@ -0,0 +1,18 @@
+// const fixNavBtns = document.querySelectorAll('.nav-button__item');
+
+// for (let index = 0; index < fixNavBtns.length; index++) {
+//   sectionsArr.push(sections[index]);
+
+//   fixNavBtns[index].onclick = e => {
+
+//     for (let i = 0; i < fixNavBtns.length; i++) {
+//       fixNavBtns[i].classList.remove('nav-button__item--active');
+//       sections[i].classList.remove('section--active');
+//     };
+
+//     e.currentTarget.classList.add('nav-button__item--active');
+//     sections[index].classList.add('section--active');
+
+//     display.style.transform = `translateY(${index * -100}%)`;
+//   };
+// };
